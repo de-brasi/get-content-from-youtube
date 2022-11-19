b433f6b76fca52b1acbbac85be3d9b8950fb6550
de_brasi <ya.kakadu.ru@yandex.ru>
2022-11-19 21:56:43 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+1) Get audio from YouTube video.
+2) Slice one audio by part with time points.
+"""
+
+
+def get_audio_by_url(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    pass
