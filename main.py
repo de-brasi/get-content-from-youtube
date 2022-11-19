0e19b92766ea80319755065741b269e8d678b9e5
de_brasi <ya.kakadu.ru@yandex.ru>
2022-11-19 23:11:47 +0300

    Catch exception

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ STORAGE_DIR_NAME = "content_storage"
 STORAGE_DIR_PATH = CUR_PATH + '/' + STORAGE_DIR_NAME
 
 
-def get_audio_by_url(users_url: str) -> int:
-    video = pytube.YouTube(users_url)
-    video_streams = video.streams.filter(file_extension="mp4").filter(type="audio")
-    streams_sorted_by_abr = video_streams.order_by('abr')[::-1]
-    if streams_sorted_by_abr:
-        try:
-            streams_sorted_by_abr[0].download(STORAGE_DIR_PATH)
+def get_audio_by_url(users_url: str, destination_directory_: str) -> int:
+    try:
+        video = pytube.YouTube(users_url)
+        video_streams = video.streams.filter(file_extension="mp4").filter(type="audio")
+        streams_sorted_by_abr = video_streams.order_by('abr')[::-1]
+        if streams_sorted_by_abr:
+            streams_sorted_by_abr[0].download(destination_directory_)
             return SUCCESS
-        except ...:
-            pass
+    except ...:
+        pass
+
     return FAILURE
 
 
@@ -33,4 +34,4 @@ if __name__ == '__main__':
     while not validators.url(url_):
         print("Invalid URL. Get me new: ")
         url_ = input()
-    get_audio_by_url(url_)
+    get_audio_by_url(url_, STORAGE_DIR_PATH)
